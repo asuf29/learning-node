68ab2558f0edaf1de837c34afbee528fc4520eb0
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2022-10-14 16:51:55 +0300

    I learned what is fetch

--- callback.js
+++ callback.js
@@ -17,3 +17,20 @@
 //     console.log("Hi!");
 // });
 
+import fetch from "node-fetch";
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(data => data.json())
+.then((users) => {
+    console.log("users uploaded", users);
+
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((data) => data.json())
+        .then((post) => { 
+            console.log("post 1 uploaded",post);
+
+            fetch("https://jsonplaceholder.typicode.com/posts/2")
+            .then((data) => data.json())
+            .then((data) => console.log("post 2 uploaded", data));
+    });
+});
