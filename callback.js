3ec30bdd992e1d331ac1b28c3742e746a6eff429
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2022-10-14 16:41:44 +0300

    callback.js codes written

---
+++ callback.js
@@ -0,0 +1,19 @@
+//vereceğimiz parametredeki süre tamamlandıktan sonra 1 kere çalışır
+// setTimeout(() => {
+//     console.log("hello");
+// }, 2000);
+
+// setInterval(() => {
+//     console.log("Hello, I will work every second");
+// }, 1000);
+
+//callback ile bu fonk çalıştırıyoruz
+// const sayHi = (cb) => {
+//     cb();
+// };
+
+//parametre olarak bir fonk verdik
+// sayHi(() => {
+//     console.log("Hi!");
+// });
+
