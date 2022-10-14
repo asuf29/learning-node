ab3fd3cc59945d8b1b4e4a8e12d952f3682d1acb
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2022-10-14 16:28:55 +0300

    I wrote my first module

---
+++ my-module.js
@@ -0,0 +1,28 @@
+const hello = (name) => {
+    console.log(`Hello ${name}`);
+};
+
+const sum = (a, b) => a + b;
+
+const substract = (a, b) => a - b;
+
+const text = "Text";
+
+const user = {
+    name: "Asude",
+    surname: "Fışkın",
+    age: 21,
+};
+
+const users = [{
+    name: "Damla",
+    surname: "Kasal",
+    age: 21,
+}, 
+{   
+    name: "Alp",
+    surname: "Boyacıoğlu",
+    age: 2,
+}];
+
+export {hello, sum, subtract, text, user, users};
